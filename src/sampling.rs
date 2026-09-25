bac82c1c03b8a12f5569ada2caa13a9878c5d822
Ivar Stangeby <istangeby@gmail.com>
2026-09-25 19:19:29 +0200

    add linear_pdf

--- src/sampling.rs
+++ src/sampling.rs
@@ -7,14 +7,29 @@ fn lerp(x: PatinaFloat, a: PatinaFloat, b: PatinaFloat) -> PatinaFloat {
     (1.0 - x) * a + x * b
 }
 
+// Return the probability distribution of the linear interpolation.
+fn linear_pdf(x: PatinaFloat, a: PatinaFloat, b: PatinaFloat) -> PatinaFloat {
+    // If we're outside the domain of lerp, return 0
+    if !(0.0..=1.0).contains(&x) {
+        0.0
+    } else {
+        2.0 * lerp(x, a, b) / (a + b)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use approx::assert_relative_eq;
 
-    use crate::sampling::lerp;
+    use crate::sampling::{lerp, linear_pdf};
 
     #[test]
     fn linear_interpolation() {
         assert_relative_eq!(0.5, lerp(0.5, 0.0, 1.0));
+    }
+
+    #[test]
+    fn test_linear_pdf() {
+        assert_relative_eq!(1.0, linear_pdf(0.5, 0.0, 1.0))
     }
 }
